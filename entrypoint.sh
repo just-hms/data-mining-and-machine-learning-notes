5a8b844baf53d8e0c1097452951fed30ec7209f0
just-hms <just-hms@users.noreply.github.com>
2023-01-28 16:08:30 +0000

    deploy: 594e7f3c11e75cbbe23eafe48b57675e238171f9

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
+mkdir build -p
+
 # create un bundle.md
 files=$(ls docs/[0-9]-* ; ls docs/[0-9][0-9]-*)
 
-echo "" > bundle.md
+echo "" > build/bundle.md
 
 for file in $files; do
-    cat "$file" >> bundle.md
-    echo "\n\n" >> bundle.md
+    cat "$file" >> build/bundle.md
+    echo "\n\n" >> build/bundle.md
 done
 
 # convert to html
 pandoc \
     --toc --standalone --verbose --mathjax \
-    -f markdown -t html bundle.md \
-    -o bundle.html --template=./template.html \
+    -f markdown -t html build/bundle.md \
+    -o build/index.html --template=./template.html \
     --metadata title="Data mining and Machine Learning" \
     --number-sections --toc-depth=3 \
     --lua-filter=adjust_number_depth.lua
 
-rm bundle.md
-
+rm build/bundle.md
