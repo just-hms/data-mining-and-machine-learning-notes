594e7f3c11e75cbbe23eafe48b57675e238171f9
just-Hms <alessandroversari1@gmail.com>
2023-01-28 17:08:01 +0100

    test github pages

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,5 +22,3 @@ pandoc \
     --lua-filter=adjust_number_depth.lua
 
 rm build/bundle.md
-
-echo "<html lang=\"en\"><head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>redirect</title><meta http-equiv=\"refresh\" content=\"0; URL='./build/'\" /></head><body></body></html>\" > index.html
